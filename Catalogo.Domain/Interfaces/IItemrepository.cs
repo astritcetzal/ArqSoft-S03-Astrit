d6e70befc00c17c02376603f1452097d16e74442
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-05-19 13:12:52 -0600

    CREATE: Domain - Item, ItemRepository

---
+++ Catalogo.Domain/Interfaces/IItemrepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalogo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogo.Domain.Interfaces
+{
+    internal interface IItemRepository
+    {
+        List<Item> ObtenerTodos();
+        Item? ObtenerPorId(int id);
+        void Agregar(Item item);
+        void Eliminar(int id);
+    }
+}
